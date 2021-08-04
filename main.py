81388f80136a0df560755eb218323201bb7e22b3
mtlmh34 <61283695+mtlmh34@users.noreply.github.com>
2021-08-04 20:18:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -974,5 +974,4 @@ def showQuarantine():
 
 # to run application
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=8080, debug=False)
-    app.run()
+    app.run(host='0.0.0.0', port=8080, debug=False)
